90bf6e279401b5219533b7860ffd03d00a15d0d9
2¥ <Yaya0312@users.noreply.github.com>
2019-08-23 10:48:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    //init
+    $("ul").on("click", ".fa-trash", function(){
+        $(this).parent().remove();
+    })
+
+    $('ul').on("click", ".item", function(){
+        $(this).toggleClass("completed");
+    })
+
+    $(".fa-plus").click( function(){
+        add();
+    })
+
+    $('input').keyup(function(e){
+        if(e.keyCode == 13) {
+           add();
+        }
+    });
+})
+
+function add(){
+    let text = $(".add-text").val().toUpperCase();
+    if (text != '') {
+        const struct = '<li><i class="fas fa-trash"></i><span class="item">' + text + '</span></li>';
+        $("#content").append(struct);
+        $(".add-text").val('');
+    }
+}
